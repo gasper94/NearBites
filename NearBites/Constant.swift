2d7d71427d9c2014760f60287b0f5ae9f9725593
gasper94 <ulimarmol94@gmail.com>
2019-05-31 10:33:22 -0700

    Add files via upload

---
+++ NearBites/Constant.swift
@@ -0,0 +1,19 @@
+//
+//  Constant.swift
+//  NearBites
+//
+//  Created by Ulises Martinez on 11/20/18.
+//  Copyright © 2018 Paul Ancajima. All rights reserved.
+//
+
+import Foundation
+
+
+class Constant {
+    
+    var APIKey: String = "ojQ-3B2hMEjQAdrZnnppDbsbJWJ6lJcoZor6OpNvL8PJ3Sxwxz3Kf-89JQqw_2dA4GclwG4LWch8RvxY65I5FlVnuwoPILw5LMWT99LJfQEmPauYBc40kCphm3jrW3Yx"
+    
+    init() {
+        APIKey = "ojQ-3B2hMEjQAdrZnnppDbsbJWJ6lJcoZor6OpNvL8PJ3Sxwxz3Kf-89JQqw_2dA4GclwG4LWch8RvxY65I5FlVnuwoPILw5LMWT99LJfQEmPauYBc40kCphm3jrW3Yx"
+    }
+}
